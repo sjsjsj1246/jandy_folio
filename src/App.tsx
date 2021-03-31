84c8eb4266511dd4501f69ffb81c337a6c008d17
Hwang In Seo <sjsjsj1246@gmail.com>
2021-03-31 21:59:50 +0900

    Feature: add material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Nav from "./components/Nav/Nav";
 
 function App() {
   return (
     <div className="App">
       <h1>hi</h1>
+      <Nav>{["Home", "About"]}</Nav>
     </div>
   );
 }
---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,46 @@
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from "@emotion/react";
+import { Paper, Tab, Tabs, TabsClassKey } from "@material-ui/core";
+import { ClassNameMap, withStyles } from "@material-ui/styles";
+import React, { ChangeEvent, ReactNode, useState } from "react";
+
+type NavProps = {
+  /**Navigation 커스텀 스타일입니다.. */
+  classes: Partial<ClassNameMap<TabsClassKey>>;
+};
+
+const Nav = ({ classes }: NavProps) => {
+  const [position, setPosition] = useState(0);
+  return (
+    <Paper>
+      <Tabs
+        value={position}
+        onChange={(e: ChangeEvent<{}>, next: any) => {
+          setPosition(next);
+        }}
+        textColor="inherit"
+        variant="standard"
+        aria-label="nav tabs"
+        css={[navStyle]}
+        classes={{ indicator: classes.indicator }}
+      >
+        <Tab label="HOME"></Tab>
+        <Tab label="ABOUT"></Tab>
+        <Tab label="PROTFOLIO"></Tab>
+      </Tabs>
+    </Paper>
+  );
+};
+
+const navStyle = css`
+  background-color: black;
+  color: white;
+`;
+
+const styles = {
+  indicator: {
+    backgroundColor: "white",
+  },
+};
+
+export default withStyles(styles)(Nav);
